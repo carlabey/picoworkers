46e0c99dc5cb536e48ea852c5f29e155be5a0bc9
carlabey <74896997+carlabey@users.noreply.github.com>
2021-08-06 11:30:15 +0530

    Update marketing_visit_website.js

--- marketing_visit_website.js
+++ marketing_visit_website.js
@@ -4,27 +4,51 @@ This is to crawl given website according to their need.
 // first page
 let firstPage = "";
 let siteConfig = {"startPage": "https://itblog360.com/",
-                  "numberOfPagesToVisit": 10, 
+                  "numberOfPagesToVisit": 8, 
                   "timePerPage": 30,
                   "postClassName": "read-more button",
                   "nextClassName": "next page-numbers" };
 
 let postElements = document.getElementsByClassName(siteConfig.postClassName);
-let nextElement = (siteConfig.nextClassName!=="")?siteConfig.nextClassName:null;
+let nextClassName = (siteConfig.nextClassName!=="")?siteConfig.nextClassName:null;
+let nextElement = nextClassName?document.getElementsByClassName(nextClassName)[0]:null;
+let nextElementUrl = null;
 if(nextElement){
+  nextElementUrl = nextElement.href;
   console.log("Have a next element");
 }else{
   console.log("Do not have next element");
 }
 
+  console.log("Next List Page", nextElementUrl);
+
 let pages=[];
 if(sessionStorage.getItem('urls')){
-  pages = JSON.parse(sessionStorage.getItem('urls'));console.log("have saved pages");
+  pages = JSON.parse(sessionStorage.getItem('urls'));
+  console.log("have ",pages.length," saved pages");
 }else{
   console.log("no saved pages");
 }
+let pageCount = pages.length;
+Array.from(postElements).every((link)=>{
+  if(!pages.includes(link.href)){
+    console.log("Page added to list");
+    pageCount++;
+    console.log(pageCount," => ", link.href);
+    pages.push(link.href);
+    if(pages.length===siteConfig.numberOfPagesToVisit){
+      console.log("Requiered page links collected");
+      console.log("Stoped collecting page links.");
+      return false;
+    }else{
+      console.log("Need more  pages");
+    }
+  }else{
+    console.log("Page already in the list");
+  }
+  return true;
+});
 
-Array.from(postElements).forEach((plant)=>{console.log(plant.href);pages.push(plant.href)});
 let uniquePages = [...new Set(pages)];
 let jsString = JSON.stringify(uniquePages);
 sessionStorage.setItem('urls',jsString);
@@ -32,3 +56,16 @@ console.log(sessionStorage.getItem('urls'));
 let savedArray= JSON.parse(sessionStorage.getItem('urls'));
 console.log("=====================");
 console.log(savedArray);
+
+//visit next page
+if(uniquePages.length<siteConfig.numberOfPagesToVisit){
+    console.log("Page Count ", uniquePages.length, ", Required ",siteConfig.numberOfPagesToVisit," Need more pages");
+    //window.location = nextElementUrl;
+  console.log("Next List Page", nextElementUrl);
+  //click Next button
+  nextElement.click();
+}else{
+  console.log("Done! Collected requierd number of pages");
+}
+
+sessionStorage.removeItem('urls');
