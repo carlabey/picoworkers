4ac64d24505254bce9118a8bd80aab82930b8114
carlabey <74896997+carlabey@users.noreply.github.com>
2021-08-06 09:38:07 +0530

    Update marketing_visit_website.js

--- marketing_visit_website.js
+++ marketing_visit_website.js
@@ -1,12 +1,30 @@
+/*
+This is to crawl given website according to their need.
+*/
+// first page
+let firstPage = "";
+let siteConfig = {"startPage": "https://itblog360.com/",
+                  "numberOfPagesToVisit": 10, 
+                  "timePerPage": 30,
+                  "postClassName": "read-more button",
+                  "nextClassName": "next page-numbers" };
 
-var pages=[];
-if(sessionStorage.getItem('urls')){
-  pages = JSON.parse(sessionStorage.getItem('urls'));console.log("test")
+let postElements = document.getElementsByClassName(siteConfig.postClassName);
+let nextElement = (siteConfig.nextClassName!=="")?siteConfig.nextClassName:null;
+if(nextElement){
+  console.log("Have a next element");
 }else{
-  console.log("no test")
+  console.log("Do not have next element");
 }
 
-Array.from(document.getElementsByClassName("read-more button")).forEach((plant)=>{console.log(plant.href);pages.push(plant.href)});
+let pages=[];
+if(sessionStorage.getItem('urls')){
+  pages = JSON.parse(sessionStorage.getItem('urls'));console.log("have saved pages");
+}else{
+  console.log("no saved pages");
+}
+
+Array.from(postElements).forEach((plant)=>{console.log(plant.href);pages.push(plant.href)});
 let uniquePages = [...new Set(pages)];
 let jsString = JSON.stringify(uniquePages);
 sessionStorage.setItem('urls',jsString);
